6359f8edc7eed2fcd1015b27b31ed70b93bbafde
panvla <pandvlado@gmail.com>
2023-01-06 19:07:51 +0100

    frondend done.

--- backend/src/main/java/com/vladimirpandurov/employeemanager3/Employeemanager3Application.java
+++ backend/src/main/java/com/vladimirpandurov/employeemanager3/Employeemanager3Application.java
@@ -2,12 +2,28 @@ package com.vladimirpandurov.employeemanager3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Employeemanager3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Employeemanager3Application.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
