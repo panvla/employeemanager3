98affd7da926082e97c24aa39a5482b69e2279e7
panvla <pandvlado@gmail.com>
2023-01-06 13:39:00 +0100

    added initial project

---
+++ backend/src/main/java/com/vladimirpandurov/employeemanager3/Employeemanager3Application.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employeemanager3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeemanager3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeemanager3Application.class, args);
+	}
+
+}
